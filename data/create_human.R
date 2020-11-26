c1f02a279f460944c40bcc38db37b852e0dd604d
Martti Kaila <martti.kaila@helsinki.fi>
2020-11-26 13:55:20 +0200

    chapter

--- data/create_human.R
+++ data/create_human.R
@@ -3,6 +3,7 @@
 # This script performs the data wrangling part of the Week €
 
 library(dplyr) 
+library("tidyverse")
 
 # 1. Create scrip: Done! 
 
@@ -17,7 +18,7 @@ summary(hd)
 summary(gii)
 
 # 4. Rename the variables
-colnames(hd) <- c("hdiRank","country","hdi","lifeExp","eduExp","eduMean","Ggni","gniMinusRank")
+colnames(hd) <- c("hdiRank","country","hdi","lifeExp","eduExp","eduMean","gni","gniMinusRank")
 colnames(gii) <- c("giiRank","country","gii","matMor","adoBirth","Parli.F","edu2Fat","edu2Mot","laborFat","laborMot")
 
 # 5.  Create asked ratios.  
@@ -29,3 +30,43 @@ human <- inner_join(hd, gii, by = "country")
 
 setwd("~/IODS-project/data")
 write.csv(human, file = "human.csv")
+
+###########################
+#                        #
+#      WEEK 5            #
+#                        #
+##########################
+
+# 0.) Load the data and summarize. 
+# 
+human <- read.csv(file = "human.csv")
+human <- select(human,-X)
+
+glimpse(human) 
+summary(human)
+
+# So we have around 195 observations and 19 variables. The data has been created by merging together Human Development
+# index data that contains several variables measuring countries' level of development, and 
+# data on gender norms that measures , for example, how large fraction of women/men have certain education
+# level or are working. 
+
+# 1) 
+human$gni <- as.numeric(gsub(",", "", human$gni))
+
+# 2) Select the variables we need
+human <- select(human, country, edu2FM, laborFM, eduExp, lifeExp, gni, matMor, adoBirth, Parli.F )
+
+# 3) Get rid of observations with missing values. 
+human <- human %>% drop_na()
+
+# 4) Remove the observations which relate to regions instead of countries
+
+human <- human[-c(154,156:162), ]
+
+# 5) Rename the rows ,delete the country variable, and save 
+
+rownames(human) <- human$country
+human <- select(human,-country)
+
+write.csv(human, file = "human.csv")
+
